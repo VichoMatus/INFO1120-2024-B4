f854ef42f4afa97b6530654f8a329c28105a0c01
iignvcioo <rpedraza2024@alu.utc.cl>
2024-05-28 13:21:49 -0400

    Incorporacion modulo pandas-SQL

---
+++ p1.py
@@ -0,0 +1,14 @@
+import sqlite3
+import pandas as pd
+sql = sqlite3.connect("Sql_data/db_personas.db")
+curs = sql.cursor()
+df = pd.read_sql_query("SELECT nacionalidad from personas", sql)
+print(df)
+
+
+
+"""
+curs.execute("SELECT nacionalidad from personas")
+filas = curs.fetchall()
+print(filas)
+"""
