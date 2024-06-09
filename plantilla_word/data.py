4a732d57ca4229cbd9482498cc7adfa8a6453fc8
VichoMatus <vmatus2024@alu.uct.cl>
2024-06-09 00:02:28 -0400

    Revert "Merge remote-tracking branch 'origin/rodri' into vichoo"
    This reverts commit d1dfb14ef411760cb61eeb3c7d32efc4cde74c85, reversing
    changes made to 255c7c787b902e1b66817fb0eca7dd8816499192.

--- plantilla_word/data.py
+++ plantilla_word/data.py
@@ -2,37 +2,20 @@ import pandas as pd, funciones as fc
 from word_gen import example_contract
 
 
-a = "--------------------------------------------------------------------------------------------------------------------"
-
-print (a ,"\nIngrese '1' para buscar datos por rut.")
-print ("Ingrese '2' para buscar datos por rango.\n",a)
-
-
+print ("Ingrese '1' para buscar datos por rut.")
+print ("Ingrese '2' para buscar datos por rango.")
 
 
 
 try:
     rqst = int (input ("Ingrese aqui: "))
     if rqst < 1 or rqst > 2:
-        print("Ingrese '1' u '2'. \n",a)
+        print("Ingrese '1' u '2'. ")
     elif rqst == 1:
-        print("Usted ha seleccionado 'buscar datos por rut'.\n",a)
+        print("Usted ha seleccionado 'buscar datos por rut'.")
         fc.buscar_por_rut()
-    elif rqst == 2: 
-        print("Usted ha seleccionado buscar por rango \h Seleccione el rango que desea buscar:\n",a)
-        print("Ingrese 1.- Si desea buscar por indice de filas.")
-        print("Ingrese 2.- Si desea buscar por Nacionalidad. \nIngrese 3.- Si desea buscar por numero de rol.")
-        print(a)
-        choice = int(input("Ingrese aqui:"))
-        print(a)
-        if choice < 1 or choice > 3:
-            print("Ingrese una opcion valida entre '1' a '5'. \n",a)
-        if choice == 1 :
-            print("Usted ha seleccionado 'Buscar datos por Indice de filas'.\n",a)
-        if choice == 2 :
-            print("Usted ha seleccionado 'Buscar datos por Nacionalidad'.\n",a)
-        if choice == 3 :
-            print("Usted ha seleccionado 'Buscar datos por Numero de rol'.\n",a)
+    elif rqst == 2:
+        print("Funcion Rango")
 except :
-    print("Ingrese valores validos.")
+    print("Ingrese solo numeros.")
 
